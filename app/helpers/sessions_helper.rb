64c5565e2083f685c2da9a3fe0c6a7b5f3e55c4d
Dave Cole <dave@wistia.com>
2015-10-11 21:31:53 -0400

    Make it possible to log out and add testing for that

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,9 +5,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
-
   end
 
   def logged_in?
